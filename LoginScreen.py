2d25161f6cfe65ebc3f4377351ecb62c973587c7
BENBENHAIDERXANDERlouisARYANMAXOLIVER <163159670+BENBENHAIDERXANDERlouisARYANMAXOLIVER@users.noreply.github.com>
2024-09-26 16:00:09 +0100

    Add files via upload

---
+++ LoginScreen.py
@@ -0,0 +1,16 @@
+from tkinter import *
+import tkinter as tk
+root=tk.Tk()
+root.title("Login Screen")
+root.minsize(250,250)
+root.maxsize(1000,1000)
+tk.Label(root ,text="Username").grid(row=0)
+e1=tk.Entry(root)
+e1.grid(row=0, column=0)
+
+root.mainloop()
+#LOOK ON THE CLASSROOM FOR THE USER DATABASE
+#!!!!!!!!!!!!!!!!!!!!!!!!
+#!!!!!!!!!!!!!!!!!!!!!!!!
+#!!!!!!!!!!!!!!!!!!!!!!!!
+#!!!!!!!!!!!!!!!!!!!!!!!!
